9a046d9df4d24bfcd47aef3a18f20613c9eb5563
Hypnos <Hypnoscodes@users.noreply.github.com>
2024-02-10 11:53:19 +0545

    9618/42 Paper 4 Oct/Nov 2022

---
+++ 9618-42 Paper 4 OctNov 2022.py
@@ -0,0 +1,39 @@
+class character():
+    def __init__(self,name,xcord,ycord):
+        self.Name = name
+        self.XCoordinate = xcord
+        self.YCoordinate = ycord
+
+    def GetName(self):
+        return self.Name
+
+    def GetX(self):
+        return self.XCoordinate
+
+    def GetY(self):
+        return self.YCoordinate
+
+    def ChangePosition(self,XChange,YChange):
+        self.XCoordinate = self.XCoordinate + XChange
+        self.YCoordinate = self.YCoordinate + YChange
+
+filename = "D:\Python\Paganini\Characters.txt"
+ArrayCharacter=[""]*30
+x=open(filename,'r')
+for i in range(0,10):
+    ArrayCharacter[i] = x.readline()
+    ArrayCharacter[i+1]= x.readline()
+    ArrayCharacter[i+2] = x.readline()
+x.close()
+
+searchname = input("enter search name:")
+
+for j in range(0,30):
+    if ArrayCharacter[j]==searchname:
+        foundindex = j
+
+
+
+
+
+
