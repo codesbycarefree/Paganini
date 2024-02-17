1c5aeba09f7891476d4a194fde5bde3946635e65
Hypnos <Hypnoscodes@users.noreply.github.com>
2024-02-17 20:11:41 +0545

    Update Qn3.py

--- 9618/2023/42 Oct/Qn3.py
+++ 9618/2023/42 Oct/Qn3.py
@@ -21,5 +21,24 @@ class Character():
     def ReturnAge(self):
         return (2023-self.__DateOfBirth.year)
 
-    
 
+FirstCharacter = Character("Royal",datetime.datetime(2019,1,1),70,30)
+FirstCharacter.Learn()
+print("The character's name is",str(FirstCharacter.GetName())+", his age is",FirstCharacter.ReturnAge(),"and has intelligence",str(FirstCharacter.GetIntelligence())+".")
+
+class MagicCharacter(Character):
+    def __init__(self,elementp,namep,birthdatep,piq,speedp,):
+        self.__Element = elementp
+        super().__init__(self,namep,birthdatep,piq,speedp)
+
+    def Learn(self):
+        if str(self.__Element).lower() == "fire" or "water":
+            super().SetIntelligence(super().GetIntelligence()*1.2)
+        elif str(self.__Element).lower() == "earth":
+            super().SetIntelligence(super().GetIntelligence()*1.3)
+        else:
+            super().SetIntelligence(super().GetIntelligence()*1.1)
+
+FirstMagicCharacter = MagicCharacter("fire","Light",datetime.datetime(2018,3,3),75,22)
+FirstMagicCharacter.Learn()
+print("The character's name is",str(FirstMagicCharacter.GetName())+", his age is",FirstMagicCharacter.ReturnAge(),"and has intelligence",str(FirstMagicCharacter.GetIntelligence())+".")
