dbfce3dab540c1119bdfbfabfe8d7b363adc444e
codesbycarefree <65530197+codesbycarefree@users.noreply.github.com>
2024-02-19 17:01:42 +0545

    Update Insertion sort.py

--- 9618/Sorting algorithms/Insertion sort.py
+++ 9618/Sorting algorithms/Insertion sort.py
@@ -1,18 +1,1 @@
-Array = [12,11,13,5,6]
 
-def InsertionSort(Array):
-    for i in range(1,len(Array)):
-        key = Array[i]
-
-        j=i-1
-        while j >=0 and key < Array[i]:
-            Array[j+1]=Array[j]
-            j -= 1
-        Array[j+1] = key
-
-    print(Array)
-
-
-
-print(Array)
-print("Sorted Array")
